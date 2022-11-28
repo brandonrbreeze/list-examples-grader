6dff3e6d5431e9b06c2e46161a8cad0d6a50f470
brandonrbreeze <bbreeze@ucsd.edu>
2022-11-27 16:32:43 -0800

    stuff

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,59 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
+
+import java.util.List;
+import java.util.LinkedList;
+
 
 public class TestListExamples {
   // Write your grading tests here!
+
+  @Test
+  public void testfilter(){
+    List<String> actual= new LinkedList<>();
+    List<String> expected= new LinkedList<>();
+    StringChecker sc = new StringCheck();
+    actual.add("bacon");
+    actual.add("baconbits");
+    actual.add("letsgetbacon");
+    expected.add("baconbits");
+    expected.add("letsgetbacon");
+    actual = ListExamples.filter(actual, sc);
+    assertArrayEquals(expected.toArray(), actual.toArray());
+
+
+  }
+
+  @Test
+  public void testmerge(){
+    List<String> actual1= new LinkedList<>();
+    List<String> actual2= new LinkedList<>();
+    actual1.add("A");
+    actual1.add("C");
+    actual1.add("E");
+    actual2.add("B");
+    actual2.add("D");
+    actual1 = ListExamples.merge(actual1,actual2);
+    List<String> expected= new LinkedList<>();
+    expected.add("A");
+    expected.add("B");
+    expected.add("C");
+    expected.add("D");
+    expected.add("E");
+    assertArrayEquals(expected.toArray(), expected.toArray());
+  }
+}
+
+class StringCheck implements StringChecker{
+  
+public boolean checkString(String s){
+  if(s.length() > 5){
+    return true;
+  }
+  return false;
+}
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,37 @@
 # Create your grading script here
 
-set -e
-
 rm -rf student-submission
 git clone $1 student-submission
 
+
+cp TestListExamples.java student-submission
+
+cp -r ./lib student-submission
+
+
+
+cd student-submission
+
+if [[ -f "./ListExamples.java" ]]
+
+    then
+        echo "File found"
+
+    else 
+        echo "File not found"
+        exit 1
+
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+if [[ $? -eq 0 ]]
+    then 
+        echo "succeeded"
+    else
+     echo "failed"
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > results.txt
+
+grep "Tests run" results.txt
